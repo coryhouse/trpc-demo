5768862cc64567456082c16ed19b55c9399af150
Cory House <housecor@gmail.com>
2022-07-17 12:03:56 -0500

    Configure tRPC for client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
 import "./index.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { trpc } from "./trpc";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: client</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+const client = new QueryClient();
+
+function AppContent() {
+  const hello = trpc.useQuery(["hello"]);
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>{JSON.stringify(hello.data)}</div>
+    </div>
+  );
+}
+
+function App() {
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      // In real app, set the base URL via an env var.
+      // All trpc calls start with this base URL.
+      url: "http://localhost:8080/trpc",
+    })
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={client}>
+      <QueryClientProvider client={client}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
+
 ReactDOM.render(<App />, document.getElementById("app"));
