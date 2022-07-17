d3ebdb59b4bfb0a80198cc240d96e820f70622db
Cory House <housecor@gmail.com>
2022-07-17 15:54:31 -0500

    Implement message

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -2,12 +2,51 @@ import express from "express";
 import * as trpc from "@trpc/server";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import cors from "cors";
+import { z } from "zod";
 
-const appRouter = trpc.router().query("hello", {
-  resolve() {
-    return "Hello world";
-  },
-});
+type ChatMessage = {
+  user: string;
+  message: string;
+};
+
+const messages: ChatMessage[] = [
+  { user: "user1", message: "Hello" },
+  { user: "user2", message: "Hi" },
+];
+
+const defaultNumMessagesToReturn = 10;
+
+const appRouter = trpc
+  .router()
+  .query("hello", {
+    resolve() {
+      return "Hello world";
+    },
+  })
+  .query("getUser", {
+    resolve() {
+      return {
+        id: 1,
+        name: "Cory",
+      };
+    },
+  })
+  .query("getMessages", {
+    input: z.number().default(defaultNumMessagesToReturn),
+    resolve({ input }) {
+      return messages.slice(-input);
+    },
+  })
+  .mutation("addMessage", {
+    input: z.object({
+      user: z.string(),
+      message: z.string(),
+    }),
+    resolve({ input }) {
+      messages.push(input);
+      return input;
+    },
+  });
 
 // Export so we can access it on the client. This is the "t" in tRPC. :)
 export type AppRouter = typeof appRouter;
@@ -24,10 +63,6 @@ app.use(
     createContext: () => null,
   })
 );
-
-app.get("/", (req, res) => {
-  res.send("Hello from api-server");
-});
 
 app.listen(port, () => {
   console.log(`api-server listening at http://localhost:${port}`);
