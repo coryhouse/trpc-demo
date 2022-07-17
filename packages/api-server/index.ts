e5f04bcd406caea0d07cbe67b8b90c739c409aa3
Cory House <housecor@gmail.com>
2022-07-17 11:35:55 -0500

    Config routes

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,7 +1,26 @@
 import express from "express";
+import * as trpc from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import cors from "cors";
+
+const appRouter = trpc.router().query("hello", {
+  resolve() {
+    return "Hello world";
+  },
+});
 
 const app = express();
+app.use(cors());
 const port = 8080;
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    // Context is useful for auth, but leaving empty for now
+    createContext: () => null,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from api-server");
